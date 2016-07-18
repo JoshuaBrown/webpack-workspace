7eac76efae93937e6f1d8af13522d8fa12a05af2
Joshua Brown <jdbvt@vt.edu>
2016-07-17 23:10:15 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: {
+		index: path.join(__dirname, 'src', 'index.js')
+	},
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: '[name].js'
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				include: [
+					path.join(__dirname, 'src')
+				],
+				test: /\.js$/,
+				query: {
+					presets: ['es2015']
+				}
+			
+			},
+			{
+				test: /\.scss$/,
+				loader: 'style-loader!css-loader!postcss-loader!sass-loader',
+				include: path.join(__dirname, 'style')
+			}	
+		]
+	}
+};
