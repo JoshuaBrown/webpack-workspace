33d684f5e828a0c780b8cad018f91c361a26126b
Joshua Brown <jdbvt@vt.edu>
2016-09-15 19:05:19 -0400

    [+] Set up for react/redux app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-// main
+import React from 'react';
+import ReactDom from 'reactDom';
+import { Provider } from 'react-redux';
+import { createStore, applyMidddleware } from 'redux';
+import rootReducer from './rootReducer';
+
+// const container = document.querySelector();
+// const createStoreWithMiddleWare = applyMidddleware()(createStore);
+
+/*ReactDom.render(
+    <Provider store={createStoreWithMiddleWare(rootReducer)}>
+    </Provider>, container);
+);*/
